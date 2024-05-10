f805775a515c01a4929db43848d6daecb82144e9
anaflvialo <anaflvialo@gmail.com>
2024-05-09 22:00:57 -0300

    feat: add metrics and selectbox to create the plot

--- streamlit/main.py
+++ streamlit/main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from utils import rfmv, elbow_method, kmeans, plot_segmentation
 
+st.set_page_config(page_title='Customer Segmentation', layout="wide")
+
 st.title('Customer Segmentation')
 
 uploaded_file = st.file_uploader(
@@ -19,20 +21,50 @@ if(uploaded_file):
 
     df["order_purchase"] = pd.to_datetime(df["order_purchase"]).dt.date
 
-    df_rfmv = rfmv.get_rfmv(df)
-    df_rfmv_norm = rfmv.get_rfmv_normalized(df)
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.metric("Valor total da venda (R$)", df["monetary"].sum())
 
-    inertias = elbow_method.get_inertias(df_rfmv_norm)
-    n_clusters = elbow_method.get_optimal_number_of_clusters(inertias)
+    with col2:
+        st.metric("Quantidade de clientes", len(df["customer_unique_id"].unique()))
 
-    clusters = kmeans.apply_kmeans(df_rfmv_norm, n_clusters)
+    with col3:
+        st.metric("Quantidade de itens vendidos", len(df["product_id"].unique()))
 
-    df_rfmv["segmentation"] = clusters 
+    with col4:
+        st.metric("Quantidade de categorias vendidas", len(df["product_category_name"].unique()))
+
+    with st.status("Criando segmentação", expanded=True):
+        st.write("Calculando as métricas por cliente...")
+        df_rfmv = rfmv.get_rfmv(df)
+        df_rfmv_norm = rfmv.get_rfmv_normalized(df)
+
+        st.write("Calculando o número ótimo de segmentações...")
+        inertias = elbow_method.get_inertias(df_rfmv_norm)
+        n_clusters = elbow_method.get_optimal_number_of_clusters(inertias)
+
+        st.write("Segmentando seus clientes...")
+        clusters = kmeans.apply_kmeans(df_rfmv_norm, n_clusters)
+        df_rfmv["segmentation"] = clusters 
 
     st.write(df_rfmv.head())
 
     st.write("Quantidade de clientes por segmentação")
     st.write(df_rfmv.groupby("segmentation").agg({"customer_unique_id": "count"}).rename({"customer_unique_id": "count"}, axis=1).head())
 
-    fig = plot_segmentation.plot_segmentation(df_rfmv, "recency", "frequency", "monetary")
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        cols_to_filter = set(["recency", "frequency", "monetary", "product_variety", "category_variety"])
+        sel1 = st.selectbox(label="Escolha uma coluna", options=cols_to_filter)
+
+    with col2:
+        cols_to_filter = cols_to_filter - set([sel1])
+        sel2 = st.selectbox(label="Escolha uma coluna", options=cols_to_filter)
+    
+    with col3:
+        cols_to_filter = cols_to_filter - set([sel2])
+        sel3 = st.selectbox(label="Escolha uma coluna", options=cols_to_filter)
+
+    fig = plot_segmentation.plot_segmentation(df_rfmv, sel1, sel2, sel3)
     st.plotly_chart(fig, use_container_width=True)
